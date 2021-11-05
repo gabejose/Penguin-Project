c4303e6d20a5da77937b0e4d94257555737713c8
gabejose <g.jose10xx@gmail.com>
2021-11-05 13:39:22 +0000

    added movement to pages from searchPenguin

---
+++ searchPenguin/searchPenguin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Grab parent div for card
+let parentDiv = document.querySelector('#parentDiv');
+
+// Function to create card
+let createCard = (data) => {
+    // Create elements
+    let newCard = document.createElement('div');
+    let newCardBody = document.createElement('div');
+}
